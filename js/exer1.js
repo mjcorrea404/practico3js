b8270e3353ae344e7280946f8ecaefe572333b37
Julieta Correa <mjcorrea210@gmail.com>
2023-03-20 22:59:30 -0300

    ejercicio2

--- js/exer1.js
+++ js/exer1.js
@@ -1,7 +1,5 @@
 let meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
-console.log("<ul>");
 
 for (let i = 0; i < meses.length; i++) {
-    console.log("<li>" + meses[i] + "</li>");
+    document.write("<li>" + meses[i] + "</li>");
 }
-console.log("</ul>");
